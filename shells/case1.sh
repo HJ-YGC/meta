88f67572961711e485195f46baf38e9c64836fd1
HJ-YGC <baekhj0823@naver.com>
2024-02-28 16:23:40 +0900

    studying

---
+++ shells/case1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+echo -n "input : "
+read a
+
+let b=a/10
+
+case "$b" in
+    9) echo "A";;
+    8) echo "B";;
+    7) echo "C";;
+    6) echo "D";;
+    *) echo "F";;
+esac
+
+echo "Thank you~ Bye!"
---
+++ shells/case_new.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+case "$1" in
+    ko) echo "Seoul";;
+    us) echo "Washington";;
+    cn) echo "Beijing";;
+    jp) echo "Tokyo";;
+    *) if [ "$#" = 0 ]; then
+        echo "Enter the contry name~!"
+    else
+       echo "Your Entry => "$1" is not in the list"
+fi
+esac
+
+
+
